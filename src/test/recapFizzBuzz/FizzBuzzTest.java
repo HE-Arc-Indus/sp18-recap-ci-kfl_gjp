5a617d989e101227edc7b844280e5c20f1d36318
KenfackFlavy <flaviennelafranche.kenfack@he-arc.ch>
2018-05-19 19:25:58 +0200

    add testlistsize method

--- src/test/recapFizzBuzz/FizzBuzzTest.java
+++ src/test/recapFizzBuzz/FizzBuzzTest.java
@@ -1,3 +1,20 @@
+import static org.junit.Assert.assertThat;
+import static sun.nio.cs.Surrogate.is;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+
 public class FizzBuzzTest {
 
+   @Test
+  public void testListSize(){
+     //Arrange
+     FizzBuzz fb = new FizzBuzz();
+     List<String> expected = Arrays.asList("1", "2", "3");
+     //Act
+     List<String> actual;
+     //Assert
+     assertThat(actual.size(), is(expected.size()));
+   }
 }
