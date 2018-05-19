1ccdbe43e61563ec5079eb0f30509ccd041bb9d7
KenfackFlavy <flaviennelafranche.kenfack@he-arc.ch>
2018-05-19 19:35:01 +0200

    add method in fizzbuzz class

--- src/test/recapFizzBuzz/FizzBuzzTest.java
+++ src/test/recapFizzBuzz/FizzBuzzTest.java
@@ -1,8 +1,11 @@
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static sun.nio.cs.Surrogate.is;
 
 import java.util.Arrays;
 import java.util.List;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class FizzBuzzTest {
@@ -11,9 +14,9 @@ public class FizzBuzzTest {
   public void testListSize(){
      //Arrange
      FizzBuzz fb = new FizzBuzz();
-     List<String> expected = Arrays.asList("1", "2", "3");
+     List<String> expected = Arrays.asList(null,null,null);
      //Act
-     List<String> actual;
+     List<String> actual = fb.generateList(3);
      //Assert
      assertThat(actual.size(), is(expected.size()));
    }
